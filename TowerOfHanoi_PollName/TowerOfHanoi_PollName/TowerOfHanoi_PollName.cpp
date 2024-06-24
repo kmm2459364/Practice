090fda193bb5411efd525bf9bfa69557dcd1a2d4
関栄翔 <kmm2459364@stu.o-hara.ac.jp>
2024-06-24 11:31:38 +0900

    a

---
+++ TowerOfHanoi_PollName/TowerOfHanoi_PollName/TowerOfHanoi_PollName.cpp
@@ -0,0 +1,58 @@
+//=========================================================
+//	ハノイの塔　解法
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>   // printf(),scanf()
+const char* strFrom = getPollName(from);
+const char* strTo = getPollName(to);
+
+typedef enum {
+	A =1,
+	B =2,
+	C =3,
+} POS;
+
+char *getPollName(POS poll)
+{
+	if (poll == A)
+	{
+		return "A";
+	}
+	else if (poll == B) {
+		return "B";
+	}
+	else
+	{
+		return "C";
+	}
+}
+// 関数プロトタイプ
+void move(int no, POS from, POS to);
+
+int main()
+{
+	while (true) {
+		int n;
+		do {
+			printf("ハノイの塔\a円盤の数:");
+			scanf("%d", &n);
+		} while (n < 1);
+
+		move(n, A, C);
+	}
+	return 0;
+}
+const char* strFrom = getPollName(from);
+const char* strTo = getPollName(to);
+
+void move(int no, POS from, POS to)
+{
+	POS tmp = (POS)((A + B + C) - from - to);
+	if (no > 1) {
+		move(no - 1, from, tmp);
+	}
+	printf("円盤[%d]を %s軸から %s軸へ移動\n", no, strFrom, strTo);
+	if (no > 1) {
+		move(no - 1, tmp, to);
+	}
+}
