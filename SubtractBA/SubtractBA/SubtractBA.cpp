764bc7b8b09f74d0ec9f70d935a91096cb843266
関栄翔 <kmm2459364@stu.o-hara.ac.jp>
2024-07-05 14:04:33 +0900

    プロジェクト登録

---
+++ SubtractBA/SubtractBA/SubtractBA.cpp
@@ -0,0 +1,27 @@
+//==========================================================
+//	差分b-a
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> // sscanf(),scanf(),printf()
+// 関数プロトタイプ
+
+int main(int argc, char* argv[])
+{
+	int a, b;
+	do {
+		printf("aの値:");
+		scanf("%d", &a);
+	} while (a <= 0);
+	do {
+		printf("bの値:");
+		scanf("%d", &b);
+		if(b <= a)
+		puts("aより大きな値を入力せよ！");
+	} while (b <= a);
+	
+	// ここをコーディングしてください。
+	// b<=a なら再度入力させます。
+
+	printf("%d - %d = %d\n", b, a, b - a);
+	return 0;
+}
---
+++ SumOfFromTo/SumOfFromTo/SumOfFromTo.cpp
@@ -0,0 +1,42 @@
+﻿//==========================================================
+//	a〜bの総和
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> // sscanf(),scanf(),printf()
+// 関数プロトタイプ
+int SumOfFromTo(int from, int to);
+void calcSum(int a, int b);
+
+int main(int argc, char* argv[])
+{
+	int a, b;
+	if (argc >= 3) {
+		sscanf(argv[1], "%d", &a);
+		sscanf(argv[2], "%d", &b);
+		calcSum(a, b);
+	}
+	else {
+		while (true) {
+			printf("aの値:");
+			scanf("%d", &a);
+			printf("bの値:");
+			scanf("%d", &b);
+			calcSum(a, b);
+		}
+	}
+	return 0;
+}
+void calcSum(int a, int b)
+{
+	int sum = SumOfFromTo(a, b);
+	printf("%d〜%dの総和は%d\n", a, b, sum);
+}
+
+int SumOfFromTo(int from, int to)
+{
+	for (int i = 0; i < ; i++)
+		to+(to+1);
+	return to;
+	// ここをコーディングしてください。
+	// from<to の場合も対応してください。
+}
